59f978858f07bbffcc5a56b1668bd6ae0f9e432f
samnurollahi <sam.nurollahi@gmail.com>
2026-09-03 14:30:32 +0330

    access point

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
-#include <Wire.h> //* for I2C communication
+
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -7,16 +11,64 @@
 #include "helper/init.hpp"
 #include "helper/oled/loading.hpp"
 
+#define ssid "samRes"
+#define password "123456789"
+
+bool isLoading = false;
+
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
+WiFiServer server(3000);
+
 OledLoader loader(oled);
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
+    initOled(oled);
 
-  initOled(oled); //* initialize the OLED display
+    loader.update();
+    bool apStarted = WiFi.softAP(ssid, password);
+    if (!apStarted) {
+        Serial.println("SoftAP failed!");
+
+        oled.clearDisplay();
+        oled.setTextSize(1);
+        oled.setCursor(30, 30);
+        oled.println("AP Failed");
+        oled.display();
+
+        return;
+    }
+
+    IPAddress myip = WiFi.softAPIP(); 
+    server.begin();
+
+    Serial.println("Server started!");
+    Serial.print("AP IP address: ");
+    Serial.println(myip);
+
+
+    oled.clearDisplay();
+    oled.setCursor(25, 20);
+    oled.println("Server Ready");
+    oled.setCursor(20, 35);
+    oled.println(myip);
+    oled.display();
+
 }
 
-
 void loop() {
-  loader.update();
-} 
+
+  if(isLoading) 
+    loader.update();
+  else {
+
+    float temperature = temperatureRead();
+    Serial.println(temperature);
+    oled.setTextSize(1);
+    oled.setCursor(3, 55);
+    oled.print((String)temperature);
+    oled.print(" C");
+    oled.display();
+
+  }
+}
