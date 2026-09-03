c347a3f5feb867c4a64dcdc07f0b47d775b2617f
samnurollahi <sam.nurollahi@gmail.com>
2026-09-03 15:47:56 +0330

    file system handle

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WiFiAP.h>
+#include <WebServer.h>
+#include <LittleFS.h>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -11,64 +12,95 @@
 #include "helper/init.hpp"
 #include "helper/oled/loading.hpp"
 
-#define ssid "samRes"
-#define password "123456789"
+// #define ssid "nokt"
+// #define password "samn1111"
+#define ssid "D-Link"
+#define password "4209368970"
 
 bool isLoading = false;
 
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
-WiFiServer server(3000);
+
+WebServer server(3000);
 
 OledLoader loader(oled);
 
-void setup() {
-    Serial.begin(9600);
-    initOled(oled);
+void setup()
+{
+  Serial.begin(9600);
+  initOled(oled);
 
+  loader.update();
+  if (!LittleFS.begin(true))
+  {
+    Serial.println("LittleFS Mount Failed!");
+    return;
+  }
+
+  Serial.println("LittleFS mounted!");
+
+  File root = LittleFS.open("/");
+
+  File file = root.openNextFile();
+
+  while (file)
+  {
+    Serial.print("FILE: ");
+    Serial.print(file.name());
+    Serial.print(" | SIZE: ");
+    Serial.println(file.size());
+
+    file = root.openNextFile();
+  }
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
     loader.update();
-    bool apStarted = WiFi.softAP(ssid, password);
-    if (!apStarted) {
-        Serial.println("SoftAP failed!");
+  }
 
-        oled.clearDisplay();
-        oled.setTextSize(1);
-        oled.setCursor(30, 30);
-        oled.println("AP Failed");
-        oled.display();
+  Serial.println();
+  Serial.println("WiFi connected!");
 
-        return;
-    }
+  server.serveStatic("/", LittleFS, "/");
 
-    IPAddress myip = WiFi.softAPIP(); 
-    server.begin();
+  server.begin();
+  Serial.println("Server started!");
 
-    Serial.println("Server started!");
-    Serial.print("AP IP address: ");
-    Serial.println(myip);
+  IPAddress myip = WiFi.localIP();
+  Serial.print("ESP32 IP address: ");
+  Serial.println(myip);
 
-
-    oled.clearDisplay();
-    oled.setCursor(25, 20);
-    oled.println("Server Ready");
-    oled.setCursor(20, 35);
-    oled.println(myip);
-    oled.display();
-
+  oled.clearDisplay();
+  oled.setCursor(25, 20);
+  oled.println("Server Ready");
+  oled.setCursor(20, 35);
+  oled.println(myip);
+  oled.display();
 }
 
-void loop() {
+void loop()
+{
+  server.handleClient();
 
-  if(isLoading) 
+  if (isLoading)
+  {
     loader.update();
-  else {
-
+  }
+  else
+  {
     float temperature = temperatureRead();
-    Serial.println(temperature);
+
     oled.setTextSize(1);
     oled.setCursor(3, 55);
-    oled.print((String)temperature);
-    oled.print(" C");
-    oled.display();
 
+    oled.print(temperature);
+    oled.print(" C");
+
+    oled.display();
   }
 }
